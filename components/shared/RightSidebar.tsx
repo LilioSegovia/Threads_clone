7251ba20dc1d3cf2e9532b8a93b9bcbb6a50e811
LilioSegovia <liliodavidsegovia@gmail.com>
2023-11-30 16:08:52 -0300

    first commit

---
+++ components/shared/RightSidebar.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+
+function RightSidebar() {
+  return (
+    <section className="custom-scrollbar rightsidebar">
+      <div className="flex flex-1 flex-col justify-start">
+        <h3 className="text-heading4-medium text-light-1"> suggested communities</h3>
+      </div>
+    </section>
+  );
+}
+
+export default RightSidebar;
